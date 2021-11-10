a76260890abf438fa918a6be6f0e9cb0f95c2eef
AfitovaViktoriya <afitovaviktoria@gmail.com>
2021-11-10 14:48:16 +0300

    feat: add reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let newArr = String(n).split('');
+    let newString = '';
+
+    if (newArr[0] === '-') {
+        newArr.splice(0, 1);
+    }
+    for (let i = newArr.length - 1; i >= 0; i--) {
+        newString += newArr[i];
+    }
+
+    return Number(newString);
 }
